6bd172a34e217d02b9a7e5e6d467e47f6bf65f64
MartinL-no <martin.lorimer@icloud.com>
2023-04-09 15:29:12 +0200

    Add project files.

---
+++ OppgaveAbaxOppgave/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OppgaveAbaxOppgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
